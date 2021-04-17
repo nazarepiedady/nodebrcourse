01d87e29b8fc87347ee8701e072de61b887fb801
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-17 14:04:11 +0100

    create a signature for all functions

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -3,3 +3,15 @@
   2 - Get user's number phone from his id
   3 - Get user's address by id
  */
+
+function getUser() {
+
+}
+
+function getUserPhone(id) {
+
+}
+
+function getUserAddress(id) {
+  
+}
