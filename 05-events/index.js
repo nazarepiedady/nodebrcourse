8ce45bb34e17d5a773de4472c4fa2aa18867b42d
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-18 22:50:38 +0100

    05: attach an event on user:click to myEmitter

--- 05-events/index.js
+++ 05-events/index.js
@@ -6,3 +6,7 @@ class MyEmitter extends EventEmitter {
 
 const myEmitter = new MyEmitter();
 const nameEvent = 'user:click';
+
+myEmitter.on(nameEvent, function (click) {
+  console.log('An user clicked', click);
+});
