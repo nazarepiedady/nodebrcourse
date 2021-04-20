193cb45b825362a0139006ef5947fc4c767372fc
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-20 12:37:44 +0100

    06(forof): test time execution of forof approach

--- 06-lists/for-in/03-forof.js
+++ 06-lists/for-in/03-forof.js
@@ -4,9 +4,11 @@ async function main() {
   try {
     const names = [];
     const result = await service.getPeople('a');
+    console.time('Forof');
     for (person of result.results) {
       names.push(person.name);
     }
+    console.timeEnd('Forof');
     console.log(`Names`, names);
   } catch (error) {
     console.log('Internal Error', error);
