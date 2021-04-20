71fd2e80e1c698a50af2a2cdfef9b9a92cb0504d
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-20 14:41:44 +0100

    06(array): receive people and push each name into names list

--- 06-lists/array-methods/02-foreach.js
+++ 06-lists/array-methods/02-foreach.js
@@ -2,7 +2,15 @@ const service = require('./services');
 
 async function main() {
   try {
+    const names = [];
+    const { results } = await service.getPeople('a');
+    results.forEach(function (item) {
+      names.push(item.name);
+    });
+    console.log('Names:', names);
   } catch (error) {
     console.error(`An error was found`, error);
   }
 }
+
+main();
