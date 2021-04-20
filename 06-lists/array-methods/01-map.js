3fbf63ebb0d709f35e51a319288d8411ab15a1e7
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-20 14:51:10 +0100

    06(array): refactor results mapping to use arrow function

--- 06-lists/array-methods/01-map.js
+++ 06-lists/array-methods/01-map.js
@@ -3,9 +3,7 @@ const service = require('./services');
 async function main() {
   try {
     const { results } = await service.getPeople('a');
-    const names = results.map(function (item) {
-      return item.name;
-    });
+    const names = results.map((item) => item.name);
     console.log('Names:', names);
   } catch (error) {
     console.error(`An error was found`);
