3da4ebe87933e2000c3cf78d28f8c58a1b6780e0
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-20 14:55:48 +0100

    06(array): create customized array map function

--- 06-lists/array-methods/01-map.js
+++ 06-lists/array-methods/01-map.js
@@ -1,5 +1,14 @@
 const service = require('./services');
 
+Array.prototype.cMap = function (callback) {
+  const newArray = [];
+  for (let i = 0; i <= this.length - 1; i++) {
+    const result = callback(this[i], i);
+    newArray.push(result);
+  }
+  return newArray;
+};
+
 async function main() {
   try {
     const { results } = await service.getPeople('a');
