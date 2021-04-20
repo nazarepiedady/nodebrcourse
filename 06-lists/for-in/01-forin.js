08487198e01486f69d9ee2b006ffd4e85f6072b4
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-20 12:23:16 +0100

    06(forin): test how long take to execute the for of

--- 06-lists/for-in/01-forin.js
+++ 06-lists/for-in/01-forin.js
@@ -4,10 +4,12 @@ async function main() {
   try {
     const names = [];
     const result = await service.getPeople('a');
+    console.time('For');
     for (let i = 0; i <= result.results.length - 1; i++) {
       const person = result.results[i];
       names.push(person.name);
     }
+    console.timeEnd('For');
     console.log(`Names`, names);
   } catch (error) {
     console.log('Internal Error', error);
