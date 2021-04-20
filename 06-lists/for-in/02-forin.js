dc061e30bd36f068694f1100cc2f2e19416bada5
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-20 12:32:20 +0100

    06(forin): test time of execution of forin soluction

--- 06-lists/for-in/02-forin.js
+++ 06-lists/for-in/02-forin.js
@@ -4,10 +4,12 @@ async function main() {
   try {
     const names = [];
     const result = await service.getPeople('a');
+    console.time('Forin');
     for (let i in result.results) {
       const person = result.results[i];
       names.push(person.name);
     }
+    console.timeEnd('Forin');
     console.log(`Names`, names);
   } catch (error) {
     console.log('Internal Error', error);
