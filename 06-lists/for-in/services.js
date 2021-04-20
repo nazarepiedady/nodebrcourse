1e63cdf90531aa6a294d8e72e2651538fcc529c8
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-20 11:44:22 +0100

    06(forin): create getPeople async function

--- 06-lists/for-in/services.js
+++ 06-lists/for-in/services.js
@@ -1,2 +1,8 @@
 const axios = require('axios');
 const URL = `https://swapi.dev/api/people`;
+
+async function getPeople(name) {
+  const url = `${URL}/?search=${name}&format=json`;
+  const response = await axios.get(url);
+  return response.data;
+}
