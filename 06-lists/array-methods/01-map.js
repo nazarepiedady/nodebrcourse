b786c735be2dfea3b4fdf86d74e59892c0728f23
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-20 14:49:33 +0100

    06(array): create new names list from results mapping

--- 06-lists/array-methods/01-map.js
+++ 06-lists/array-methods/01-map.js
@@ -2,7 +2,14 @@ const service = require('./services');
 
 async function main() {
   try {
+    const { results } = await service.getPeople('a');
+    const names = results.map(function (item) {
+      return item.name;
+    });
+    console.log('Names:', names);
   } catch (error) {
     console.error(`An error was found`);
   }
 }
+
+main();
