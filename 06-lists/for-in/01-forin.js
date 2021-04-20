64934e9a83ca5db1cd1be1e126b61a3b62dbb735
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-20 12:15:51 +0100

    06(forin): add all returned names by api to names list
    get all people names that match with name value of getPeople
    arguments and push each one into names list, then log it's
    list

--- 06-lists/for-in/01-forin.js
+++ 06-lists/for-in/01-forin.js
@@ -2,7 +2,13 @@ const service = require('./services');
 
 async function main() {
   try {
-
+    const names = [];
+    const result = await service.getPeople('a');
+    for (let i = 0; i <= result.results.length - 1; i++) {
+      const person = result.results[i];
+      names.push(person.name);
+    }
+    console.log(`Names`, names);
   } catch (error) {
     console.log('Internal Error', error);
   }
