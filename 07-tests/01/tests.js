55b9482b39d09f01b676cf1b089a586910a4e505
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-23 14:03:45 +0100

    07(tests): test if people object are in right format

--- 07-tests/01/tests.js
+++ 07-tests/01/tests.js
@@ -1,3 +1,12 @@
 const assert = require('assert');
+const { getPeople } = require('./services');
 
-assert.ok(false);
+describe('Star Wars Tests', function () {
+  it('should fetch r2d2 in the right format', async () => {
+    const nameBase = 'r2-d2';
+    const expected = [{ name: 'R2-D2', weight: '96' }];
+
+    const result = await getPeople(nameBase);
+    assert.deepEqual(result, expected);
+  });
+});
