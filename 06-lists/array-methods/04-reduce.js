9721ace202b2066afcb771bacc720b6bfcea9289
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-23 10:37:08 +0100

    06(array): comment the use of default reduce method

--- 06-lists/array-methods/04-reduce.js
+++ 06-lists/array-methods/04-reduce.js
@@ -12,9 +12,9 @@ async function main() {
   try {
     const { results } = await getPeople('a');
     const weight = results.map((item) => parseInt(item.height));
-    const total = weight.reduce((previous, next) => {
+    /*  const total = weight.reduce((previous, next) => {
       return previous + next;
-    });
+    }); */
     console.log('Total', total);
   } catch (error) {
     console.error('There is a error', error);
