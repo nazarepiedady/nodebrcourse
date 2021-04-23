a0ff84dcc4cfeda599156b362bd2398e56e47d57
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-23 10:06:31 +0100

    06(array): get people that there is a as part his name

--- 06-lists/array-methods/03-filter.js
+++ 06-lists/array-methods/03-filter.js
@@ -2,6 +2,7 @@ const { getPeople } = require('./services');
 
 async function main() {
   try {
+    const { results } = await getPeople('a');
   } catch (error) {
     console.log('There is a error', error);
   }
