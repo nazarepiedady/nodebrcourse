d6737eba3f0edf1b3fdeb0a412336aac9dd87e23
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-23 14:09:22 +0100

    07(tests): use mapPeople in results from swapi

--- 07-tests/01/services.js
+++ 07-tests/01/services.js
@@ -5,7 +5,7 @@ const URL = `https://swapi.dev/api/people`;
 async function getPeople(name) {
   const url = `${URL}/?search=${name}&format=json`;
   const result = await get(url);
-  return result.data;
+  return result.data.results.map(mapPeople);
 }
 
 function mapPeople(item) {
