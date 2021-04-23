e716271012edb9198516bf517b6edcbf436138b9
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-23 10:28:33 +0100

    06(array): take all heights

--- 06-lists/array-methods/04-reduce.js
+++ 06-lists/array-methods/04-reduce.js
@@ -3,6 +3,7 @@ const { getPeople } = require('./services');
 async function main() {
   try {
     const { results } = await getPeople('a');
+    const weight = results.map((item) => parseInt(item.height));
   } catch (error) {
     console.log('There is a error', error);
   }
