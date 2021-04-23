64b900b0adc0143d8004dbaf1401f78cce027ad3
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-23 10:11:30 +0100

    06(array): map all names of family lars and print it

--- 06-lists/array-methods/03-filter.js
+++ 06-lists/array-methods/03-filter.js
@@ -3,10 +3,15 @@ const { getPeople } = require('./services');
 async function main() {
   try {
     const { results } = await getPeople('a');
+
     const familyLars = results.filter(function (item) {
       const result = item.name.toLowerCase().indexOf('lars') !== -1;
       return result;
     });
+
+    const names = familyLars.map((person) => person.name);
+
+    console.log(names);
   } catch (error) {
     console.log('There is a error', error);
   }
