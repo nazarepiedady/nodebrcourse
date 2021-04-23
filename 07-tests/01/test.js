dbdc76bee9db1b128b28f3a11e8188a9a10962d0
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-23 14:11:55 +0100

    07(tests): install nock

--- 07-tests/01/test.js
+++ 07-tests/01/test.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const { getPeople } = require('./services');
 
+const nock = require('nock');
+
 describe('Star Wars Tests', function () {
   it('should fetch r2d2 in the right format', async () => {
     const nameBase = 'r2-d2';
