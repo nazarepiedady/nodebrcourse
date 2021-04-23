336b722e9e3bc71704bf748762fbe497ce38e04a
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-23 10:09:54 +0100

    06(array): filter all lars family members

--- 06-lists/array-methods/03-filter.js
+++ 06-lists/array-methods/03-filter.js
@@ -3,6 +3,10 @@ const { getPeople } = require('./services');
 async function main() {
   try {
     const { results } = await getPeople('a');
+    const familyLars = results.filter(function (item) {
+      const result = item.name.toLowerCase().indexOf('lars') !== -1;
+      return result;
+    });
   } catch (error) {
     console.log('There is a error', error);
   }
