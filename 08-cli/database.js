1911741cd98cf8a3dad4adc128d1a221b873e4ea
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-25 19:30:46 +0100

    08(cli): create readFileAsync to receive readFile promisified

--- 08-cli/database.js
+++ 08-cli/database.js
@@ -1,5 +1,7 @@
 const { readFile } = require('fs');
 const { promisify } = require('util');
+
+const readFileAsync = promisify(readFile);
 class Database {
   constructor() {
     this._FILENAME = 'heroes.json';
