3f1f6d3ad7ce2f7dd419f14f1399adde8a3f516e
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-29 11:37:14 +0100

    08(cli): implement Database register method

--- 08-cli/database.js
+++ 08-cli/database.js
@@ -19,6 +19,15 @@ class Database {
     return true;
   }
 
+  async register(hero) {
+    const data = await this.getFileData();
+    const id = hero.id <= 2 ? hero.id : Date.now();
+    const heroWithId = { id, ...hero };
+    const finalData = { heroWithId, ...data };
+    const result = await this.writeFile(finalData);
+    return result;
+  }
+
   async list(id) {
     const data = await this.getFileData();
     const filteredData = data.filter((item) => (id ? item.id === id : true));
