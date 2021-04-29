4881a2e8f1095832037ad0b14af670aaab1a850a
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-04-29 11:26:58 +0100

    08(cli): promisify writeFile into writeFileAsync

--- 08-cli/database.js
+++ 08-cli/database.js
@@ -2,7 +2,7 @@ const { readFile, writeFile } = require('fs');
 const { promisify } = require('util');
 
 const readFileAsync = promisify(readFile);
-
+const writeFileAsync = promisify(writeFile);
 class Database {
   constructor() {
     this._FILENAME = 'heroes.json';
